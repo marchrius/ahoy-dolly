f5e6db35f163e0a7af35894e1e73189bfe3d2153
Matteo Gaggiano <maxxxx92@gmail.com>
2026-02-05 11:21:54 +0100

    Rename plugin from Hello Dolly to Ahoy Dolly
    - Renamed plugin to correct name
    - Added api call to retrieve phrase
    - Added sanitification of the text

--- hello.php
+++ hello.php
@@ -1,57 +1,28 @@
 <?php
 /**
- * @package Hello_Dolly
+ * @package Ahoy_Dolly
  * @version 1.7.2
  */
 /*
-Plugin Name: Hello Dolly
-Plugin URI: http://wordpress.org/plugins/hello-dolly/
-Description: This is not just a plugin, it symbolizes the hope and enthusiasm of an entire generation summed up in two words sung most famously by Louis Armstrong: Hello, Dolly. When activated you will randomly see a lyric from <cite>Hello, Dolly</cite> in the upper right of your admin screen on every page.
-Author: Matt Mullenweg
+Plugin Name: Ahoy Dolly
+Plugin URI: http://wordpress.org/plugins/ahoy-dolly/
+Description: The "Hello Dolly!" plugin but print insults in a Pirate way! When activated you will randomly see an insult from <cite>Pirate Monkeyness (https://pirate.monkeyness.com/) </cite> in the upper right of your admin screen on every page.
+Author: Matteo Gaggiano
 Version: 1.7.2
-Author URI: http://ma.tt/
+Author URI: https://github.com/marchrius
 */
 
-function hello_dolly_get_lyric() {
-	/** These are the lyrics to Hello Dolly */
-	$lyrics = "Hello, Dolly
-Well, hello, Dolly
-It's so nice to have you back where you belong
-You're lookin' swell, Dolly
-I can tell, Dolly
-You're still glowin', you're still crowin'
-You're still goin' strong
-I feel the room swayin'
-While the band's playin'
-One of our old favorite songs from way back when
-So, take her wrap, fellas
-Dolly, never go away again
-Hello, Dolly
-Well, hello, Dolly
-It's so nice to have you back where you belong
-You're lookin' swell, Dolly
-I can tell, Dolly
-You're still glowin', you're still crowin'
-You're still goin' strong
-I feel the room swayin'
-While the band's playin'
-One of our old favorite songs from way back when
-So, golly, gee, fellas
-Have a little faith in me, fellas
-Dolly, never go away
-Promise, you'll never go away
-Dolly'll never go away again";
-
-	// Here we split it into lines.
-	$lyrics = explode( "\n", $lyrics );
+function ahoy_dolly_get_lyric() {
+	// Get the text from external api
+	$text = file_get_contents('https://pirate.monkeyness.com/api/insult');
 
 	// And then randomly choose a line.
-	return wptexturize( $lyrics[ mt_rand( 0, count( $lyrics ) - 1 ) ] );
+	return wptexturize( sanitize_text_field( $text ) );
 }
 
 // This just echoes the chosen line, we'll position it later.
-function hello_dolly() {
-	$chosen = hello_dolly_get_lyric();
+function ahoy_dolly() {
+	$chosen = ahoy_dolly_get_lyric();
 	$lang   = '';
 	if ( 'en_' !== substr( get_user_locale(), 0, 3 ) ) {
 		$lang = ' lang="en"';
@@ -59,14 +30,14 @@ function hello_dolly() {
 
 	printf(
 		'<p id="dolly"><span class="screen-reader-text">%s </span><span dir="ltr"%s>%s</span></p>',
-		__( 'Quote from Hello Dolly song, by Jerry Herman:', 'hello-dolly' ),
+		__( 'Insult from Pirate Monkeyness, by <a href="https://pirate.monkeyness.com/">Tim Moses</a>. Please take this for what it is, a joke:', 'hello-dolly' ),
 		$lang,
 		$chosen
 	);
 }
 
 // Now we set that function up to execute when the admin_notices action is called.
-add_action( 'admin_notices', 'hello_dolly' );
+add_action( 'admin_notices', 'ahoy_dolly' );
 
 // We need some CSS to position the paragraph.
 function dolly_css() {
